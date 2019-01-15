d1a66512577b03054b52a03cedb09a2ba9d07913
Luke Wilson <wilson.palooza@gmail.com>
2019-01-15 11:15:28 -0800

    styled components basics, ThemeProvider using React context, media queries in styled-components

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,10 +1,35 @@
 import Link from 'next/link';
 import Nav from './Nav'
+import styled from 'styled-components'
+
+const Logo = styled.h1`
+  font-size: 4rem; 
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+  a {
+    padding: 0.5rem 1rem;
+    background: ${props => props.theme.red};
+    color: white;
+    text-transform: uppercase;
+    text-decoration: none;
+  }
+  @media (max-width: 1300px) {
+    margin: 0;
+    text-align: center; 
+  }
+`
+
 
 const Header = () => (
   <div>
     <div className="bar">
-			<a href="/">Sick Fits</a>
+			<Logo>
+			  <Link>
+          <a href="/">Sick Fits</a>
+        </Link>
+			</Logo>
       <Nav></Nav>
     </div>
 
--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -3,17 +3,62 @@
 import React, { Component } from 'react';
 import Meta from './Meta';
 import Header from './Header';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+
+
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+};
+
+// The double backtick syntax below is called tagged template literal. We're essentially 
+const StyledPage = styled.div`
+	background: white;
+	color: ${props => props.theme.black};
+`
+
+const Inner = styled.div`
+	max-width: ${props => props.theme.maxWidth};
+	margin: 0 auto;
+	padding: 2rem;
+	background: ${props => props.theme.red};
+`
 
 export default class Page extends Component {
 	render() {
 		return (
-			<div>
-				<Meta></Meta>
-				<Header></Header>
-				{this.props.children}
-			</div>
+			<ThemeProvider theme={theme}>
+				<StyledPage>
+					<Meta></Meta>
+					<Header></Header>
+					<Inner>
+						{this.props.children}
+					</Inner>
+				</StyledPage>
+			</ThemeProvider>
 		)
 	}
 }
 
 //  "rcc" then enter => gives a snippet of React component
+
+
+
+
+// TAGGED TEMPLATE LITERALS
+// From https://medium.freecodecamp.org/es6-tagged-template-literals-48a70ef3ed4d
+// const myTag = (literals, name) => {
+//   console.log('Literals', literals); //Output -> Literals [ 'Hello ', '!' ]
+//   console.log('Interpolation', name); //Output -> Interpolation Steve
+//   return 'Result from myTag';
+// };
+
+// const name = 'Steve';
+// const result = myTag `Hello ${name}!`; // << this is kind of like calling a function
+
+// console.log(result); //Output -> Result from myTag
