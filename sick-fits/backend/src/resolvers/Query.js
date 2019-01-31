e4994f6c866c50d34085dfe1383c1c2d5128f1b4
Luke Wilson <wilson.palooza@gmail.com>
2019-01-31 15:01:17 -0800

    add a dogs method to the Query object

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,14 @@
-const Query = {};
+const Query = {
+	dogs(parent, args, ctx, info) {
+		// There are four parameters for any query function:
+		// parent
+		// args: arguments that have been passed to the query.
+		// ctx: "context" - see createServer function for related stuff to context.
+		// info: More information that is available
+		return global.dogs || []
+
+	}
+
+};
 
 module.exports = Query;
