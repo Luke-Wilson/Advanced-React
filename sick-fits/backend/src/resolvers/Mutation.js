f9dd8b2096fc359c0dcf4f1b8cf89e2d44891502
Luke Wilson <wilson.palooza@gmail.com>
2019-01-31 15:01:04 -0800

    add a createDog method to the mutations object

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,12 @@
-const mutations = {};
+const mutations = {
+	createDog(parents, args, context, info) {
+		global.dogs = global.dogs || [];
+		
+		const newDog = { name: args.name };
+		global.dogs.push(newDog);
+		console.log(args)
+		return newDog;
+	}
+};
 
 module.exports = mutations;
